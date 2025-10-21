a8b89565cb7b02940b254f15945c2166da3d5100
tmnguyen170-tech <tmnguyen170@student.hbuhsd.edu>
2025-10-21 18:20:13 +0000

    Umamusume

--- Main.java
+++ Main.java
@@ -5,5 +5,19 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    for (int count = 1; count <= 25; count += 2)
+    {
+      System.out.println(count);
+    }
+
+
+    for (int Q2count = 17; Q2count <= 73; Q2count++);
+    {
+      System.out.println(count);
+      if (Q2count % 10 == 7)
+      {
+        System.out.println("\n" + Q2count);
+      }
+    }
   }
 }
