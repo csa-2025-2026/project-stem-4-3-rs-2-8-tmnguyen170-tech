e86c169d4937957fe562266686371fad4787d586
tmnguyen170-tech <tmnguyen170@student.hbuhsd.edu>
2025-10-23 17:50:44 +0000

    uma

--- Main.java
+++ Main.java
@@ -10,14 +10,29 @@ public class Main
       System.out.println(count);
     }
 
-
-    for (int Q2count = 17; Q2count <= 73; Q2count++);
+    int tenCount = 1;
+    for (int Q2count = 17; Q2count <= 73; Q2count++)
     {
-      System.out.println(count);
-      if (Q2count % 10 == 7)
+      System.out.print(Q2count + " ");
+      if (tenCount == 10)
       {
-        System.out.println("\n" + Q2count);
+        System.out.println();
+        tenCount = 1;
       }
+      tenCount++;
+    }
+
+    int Num = 23;
+    int fivecounter = 1;
+    for (int Q3counter = Num; Q3counter <= 50; Q3counter++)
+    {
+      System.out.print(Q3counter + " ");
+      if (fivecounter == 5)
+      {
+        System.out.println();
+        fivecounter = 1;
+      }
+      fivecounter++;
     }
   }
 }
